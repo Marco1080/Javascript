d2bdc9a876b583c278df9c86d5d7b733d6a80447
Marco <93841465+Marco1080@users.noreply.github.com>
2023-02-12 01:37:47 +0000

    Multiple classes added

--- eldenApi/javascript/getTalismans.js
+++ eldenApi/javascript/getTalismans.js
@@ -1,6 +1,6 @@
-export class getDataWeapons {
+export class getDataTalismans {
     static getNumberPages() {
-        return fetch('https://eldenring.fanapis.com/api/weapons')
+        return fetch('https://eldenring.fanapis.com/api/talismans')
             .then(res => res.json())
             .then(data => Math.floor((data.total)/20))
             .catch(error => console.log(`Error: ${error}`))
@@ -9,7 +9,7 @@ export class getDataWeapons {
     static async getData(pages){
         let weaponList = [];
             for(let index = 0; index <= pages; index++) {
-                await fetch(`https://eldenring.fanapis.com/api/weapons?limit=20&page=${index}`)
+                await fetch(`https://eldenring.fanapis.com/api/talismans?limit=20&page=${index}`)
                     .then(res => res.json())
                     .then(data => weaponList.push(data.data))
                     .catch(error => console.log(error))
--- eldenApi/javascript/main.js
+++ eldenApi/javascript/main.js
@@ -1,13 +1,54 @@
 import { getDataWeapons } from "./getDataWeapons.js";
+import { getDataArmors } from "./getDataArmors.js";
+import { getDataTalismans } from "./getTalismans.js";
+import { getDataBosses } from "./getDataBosses.js";
 
-async function call() {
+const container = document.querySelector('.container');
+
+async function callWeapons() {
     const pages = await getDataWeapons.getNumberPages();
-    console.log(`Numero de paginas: ${pages}`)
-    console.log('Esperando datos<---')
-    const dat = await getDataWeapons.getData(pages);
-    console.log('Mostrando datos<---');
-    getDataWeapons.showWeapons(dat, pages);
-}
-call();
+    const dataFromApi = await getDataWeapons.getData(pages);
 
-const weaponOption = document.querySelector('.weaponOption').addEventListener('click', ()=>{});
+    dataFromApi.forEach(information => {
+        information.forEach(info => {
+            let card = document.createElement('div');
+            card.classList.add('card');
+            let cardName = document.createElement('p');
+            let cardNameText = document.createTextNode(info.name);
+            cardName.appendChild(cardNameText);
+            cardName.classList.add('cardTitle');
+            card.appendChild(cardName);
+            let cardImage = document.createElement('img');
+            cardImage.src = `${info.image}`;
+            card.appendChild(cardImage);
+            let cardDescription = document.createElement('p');
+            let cardDescriptionText = document.createTextNode(info.description);
+            cardDescription.appendChild(cardDescriptionText);
+            cardDescription.classList.add('cardDescription');
+            card.appendChild(cardDescription);
+            container.appendChild(card);
+        });
+    });
+}
+const weaponOption = document.querySelector('.weaponOption').addEventListener('click', ()=>{
+    callWeapons();
+});
+
+/* async function callArmors() {
+    const pages = await getDataArmors.getNumberPages();
+    console.log(pages);
+    const data = await getDataArmors.getData(pages);
+    getDataArmors.showData(data);
+}
+callArmors(); */
+
+
+
+/* async function callArmors() {
+    const pages = await getDataTalismans.getNumberPages();
+    console.log(pages);
+    const data = await getDataTalismans.getData(pages);
+    getDataTalismans.showData(data);
+}
+callArmors(); */
+
